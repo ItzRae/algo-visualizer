a4515cb98a96e5d4c10402cf989e8ab86f4a4479
ItzRae <rachellin443@gmail.com>
2024-10-21 23:34:40 -0400

    deploy website

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -15,7 +15,8 @@ export default function Modal({onClose, selectedAlgorithm}) {
     return (
         <div ref={modalRef} onClick={closeModal} className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
             <div className="flex flex-col w-1/2 h-1/2 bg-white">
-                <button  classname='place-self-end' onClick={onClose}><X size={30}/></button>
+                <button  classname='place-self-end' onClick={onClose} color="black"><X size={30}/></button>
+                <h1 className="text-black text-center text-2xl font-semibold">{sortingAlgoData[selectedAlgorithm].name} Pseudocode</h1>
                 <CodeInterface text={sortingAlgoData[selectedAlgorithm].pseudocode}/>
             </div>
         </div>
